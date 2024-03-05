16d75caf07341e869fd3614e8fb9a995c74a91cc
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-03-05 16:04:29 +1100

    Updating the collectEmployees function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,81 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
+
 // Collect employee data
-const collectEmployees = function() {
+const collectEmployees = function () {
+
+
+
+  // Declared and initialized an employee array
+  const employeesArray = [];
+  console.log(`employeesArray ${employeesArray}`);
+
   // TODO: Get user input to create and return an array of employee objects
+
+  let keepAddingEmployee = true;
+  let addEmployee = true;
+
+  while (keepAddingEmployee) {
+    // Declared and initialized an employee object
+    let employee = {
+      // Properties are made up of key-value pairs
+      firstName: "",
+      lastName: "",
+      salary: 0,
+    };
+
+
+    if (addEmployee) {
+      let userInputFirstName = window.prompt("Enter first name:");
+      console.log(`userInputFirstName --> ${userInputFirstName}`);
+      let userInputLastName = window.prompt("Enter last name:");
+      console.log(`userInputLastName --> ${userInputLastName}`);
+      let userInputSalary = window.prompt("Enter salary:");
+      console.log(userInputSalary);
+
+
+
+      if (userInputFirstName == "" || userInputLastName == "" || userInputSalary == "") {
+
+        window.alert("Invalid Entry");
+
+      }
+
+      if ((userInputFirstName == null) || (userInputLastName == null) || (userInputSalary == null)) {
+        console.log(`employeesArray--> ${employeesArray}`);
+        return employeesArray;
+      } else if (userInputFirstName != "" && userInputLastName != "" && userInputSalary != "") {   
+
+
+        console.log(`userInputFirstName 2222--> ${userInputFirstName}`);
+        employee.firstName = userInputFirstName;
+        employee.lastName = userInputLastName;
+        employee.salary = userInputSalary;
+        employeesArray.push(employee);
+        console.log(employeesArray[0]);
+        addEmployee = window.confirm("Do you want to add another employee");
+        console.log(addEmployee);
+       
+      }
+    }
+    else {
+      console.log(`employeesArraywhen add employee is false --> ${employeesArray}`);
+      return employeesArray;
+    }
+  }
+
+
+
 }
 
 // Display the average salary
-const displayAverageSalary = function(employeesArray) {
+const displayAverageSalary = function (employeesArray) {
   // TODO: Calculate and display the average salary
 }
 
 // Select a random employee
-const getRandomEmployee = function(employeesArray) {
+const getRandomEmployee = function (employeesArray) {
   // TODO: Select and display a random employee
 }
 
@@ -23,7 +86,7 @@ const getRandomEmployee = function(employeesArray) {
 */
 
 // Display employee data in an HTML table
-const displayEmployees = function(employeesArray) {
+const displayEmployees = function (employeesArray) {
   // Get the employee table
   const employeeTable = document.querySelector('#employee-table');
 
@@ -46,9 +109,9 @@ const displayEmployees = function(employeesArray) {
 
     const salaryCell = document.createElement("td");
     // Format the salary as currency
-    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
-      style:"currency",
-      currency:"USD"
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD"
     });
 
     newTableRow.append(salaryCell);
@@ -57,8 +120,10 @@ const displayEmployees = function(employeesArray) {
   }
 }
 
-const trackEmployeeData = function() {
+const trackEmployeeData = function () {
   const employees = collectEmployees();
+
+  console.log(employees);
 
   console.table(employees);
 
@@ -68,14 +133,13 @@ const trackEmployeeData = function() {
 
   getRandomEmployee(employees);
 
-  employees.sort(function(a,b) {
+  employees.sort(function (a, b) {
     if (a.lastName < b.lastName) {
       return -1;
     } else {
       return 1;
     }
   });
-
   displayEmployees(employees);
 }
 
